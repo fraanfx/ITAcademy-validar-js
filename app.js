6e7942ff89fc81325166c8c995e4d3e0bf843eee
fraanfx <fran.am95@gmail.com>
2021-07-08 19:00:48 +0200

    otro commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log('hola')
 const formLogin = document.getElementById('loginForm');
 
 const formRegister = document.getElementById('registerForm');
@@ -5,41 +6,109 @@ const formRegister = document.getElementById('registerForm');
 function loginCredentials() {
     var acumErrors = 0;
     
-    form.classList.remove('is-invalid');
+    formLogin.classList.remove('is-invalid');
 
     var inputEmail = document.getElementById('inputEmail');
     var inputPassword = document.getElementById('inputPass');
 
     if(inputEmail.value == "") {
         inputEmail.classList.add("is-invalid");
-        document.getElementById("errorEmail").textContent = "Es un campo obligatorio";
+        document.getElementById("errorEmail").textContent = "Es un camp obligatori";
         acumErrors ++;
     } else if(!validar_email(inputEmail.value)){
         inputEmail.classList.add("is-invalid");
-        document.getElementById("errorEmail").textContent = "Formato de email incorrecto";
+        document.getElementById("errorEmail").textContent = "Format de email incorrecte";
         acumErrors ++;
     }
     if(inputPassword.value == "") {
 		inputPassword.classList.add("is-invalid");
-		document.getElementById("errorPass").textContent = "Es campo es obligatorio";
+		document.getElementById("errorPassword").textContent = "Es un camp obligatori";
 		acumErrores ++;
     }
     if(acumErrors > 0){
+        console.log('no hay modal');
         return false;
     }
     else{
+        console.log('hay modal');
         return true;
     }
 
 }
 
 function registerCredentials() {
-    var acumErrorsR = 0;
+    let acumErrorsR = 0;
 
-    form.classList.remove('is-invalid');
+    formRegister.classList.remove('is-invalid');
     
-    var inputUser = document.getElementById('user');
-    var inputProvidence = document.getElementById('provincia')
+    let inputUser = document.getElementById('inputUser');
+    let inputProvidence = document.getElementById('inputProvincia');
+    let inputMail = document.getElementById('inputMail');
+    let createPass = document.getElementById('inputPassword');
+    let confirmPass = document.getElementById('inputPasswordConfirm');
+    let terms = document.getElementById('checkTerms');
+
+
+
+    if(inputUser.value == "") {
+        inputUser.classList.add("is-invalid");
+        document.getElementById("errorUser").textContent = "Es un camp obligatori";
+        acumErrorsR ++;
+
+    } 
+
+    if(inputProvidence.value == ''){
+        inputProvidence.classList.add("is-invalid");
+        document.getElementById("errorProvincia").textContent = "Es un camp obligatori";
+        acumErrorsR ++;
+    }else{
+
+    }
+    if(inputMail.value == ''){
+        inputMail.classList.add("is-invalid");
+        document.getElementById("errorMail").textContent = "Es un camp obligatori";
+
+    } else if(!validar_email(inputMail.value)){
+        inputMail.classList.add("is-invalid");
+        document.getElementById("errorMail").textContent = "Email invalid";
+    }
+
+    if(createPass.value == ''){
+        createPass.classList.add("is-invalid");
+        document.getElementById("errorPass").textContent = "Es un camp obligatori";
+
+    }
+    else if(!validar_pass(createPass.value)){
+        createPass.classList.add("is-invalid");
+        document.getElementById("errorPass").textContent = "La password no cumpleix les condicions";
+
+    }
+
+    if(confirmPass.value == '') {
+        confirmPass.classList.add("is-invalid");
+        document.getElementById("errorConfirm").textContent = "Es un camp obligatori";
+    }
+    else if(confirmPass.value != createPass.value){
+        confirmPass.classList.add("is-invalid");
+        document.getElementById("errorConfirm").textContent = "Las passwords no coincideixen";
+    }
+    if(!terms.checked) {
+        terms.classList.add("is-invalid");
+        document.getElementById("errorCheck").textContent = "Es un camp obligatori";
+
+    }
+
+
+    if(acumErrorsR > 0){
+        console.log('no hay modal');
+
+        return false;
+    }
+    else{
+        console.log('hay modal');
+
+        return true;
+    }
 }
 
 
@@ -52,7 +121,17 @@ formLogin.addEventListener('blur', (event) => {
     //registerValidate();
 }, true);
 
+formRegister.addEventListener('blur', (event) => {
+	console.log(event);
+	if(event.target.value!='') event.target.classList.remove('is-invalid');
+    //registerValidate();
+}, true);
+
 function validar_email(email) {
 	var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 	return regex.test(email) ? true : false;
+}
+function validar_pass(pass) {
+	var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+	return regex.test(pass) ? true : false;
 }
